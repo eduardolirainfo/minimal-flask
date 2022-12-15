48d65daafb64faf79aa32e962ec8e37d7cf5044f
Eduardo Lira <eduardolirainfo@gmail.com>
2022-12-14 23:49:34 -0300

    rendering templates

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -7,12 +7,18 @@ app.secret_key = 'development key'
 
 @app.route("/")
 def hello():
-    return "<h1>Hello World!</h1>"
+    return render_template('index.html')
 
 
 @app.route("/user/<name>")
 def user(name):
-    return "<h1>Hello, %s!</h1>" % name
+
+    return render_template('user.html', name=name)
+    # return "<h1>Hello, %s!</h1>" % name
+    # or
+    # f"<h1>Hello, {name}!</h1>"
+    # or
+    # return "<h1>Hello, {}!</h1>".format(name)
 
 
 if __name__ == "__main__":
