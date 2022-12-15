b52702764af44f36fba9c612c90892cbac743674
Eduardo Lira <eduardolirainfo@gmail.com>
2022-12-15 12:21:23 -0300

    error handler

--- hello.py
+++ hello.py
@@ -8,6 +8,16 @@ app.secret_key = 'development key'
 bootstrap = Bootstrap(app)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
+
+
 @app.route("/")
 def hello():
     fruits = ['apple', 'banana', 'orange']
